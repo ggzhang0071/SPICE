8365281ffafc94bf441478f719863c46b3a75796
ggzhang0071 <34293109+ggzhang0071@users.noreply.github.com>
2021-04-24 16:23:08 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,26 +7,47 @@ import numpy as np
 
 
 class kangqiang(Dataset):
-    def __init__(self, root, split='train', show=False, transform=None):
+    def __init__(self, root, split='train', show=False, transform=None,labels=None):
+        train_list_file = "/git/assets/kangqiang_dataset_train.txt"
         self.image_list= []
-        names=os.listdir(root)
         self.transform = transform
         self.split = split  # train/test/unlabeled set
-        for file_name in names:
-            if file_name.endswith(".jpg"):
-                self.image_list.append(os.path.join(root,file_name))
+        self.root=root
+        names=os.listdir(root)
+        with open(train_list_file,"r") as filehandle:
+            for file_name in filehandle:
+                if file_name.endswith(".jpg"):
+                    self.image_list.append(file_name)
 
     def __len__(self):
         # 返回数据集的数据数量
         return len(self.image_list)
+    
+    def __loadfile(self, data_file):
+        print(os.path.join(self.root, data_file))
+        path_to_data = os.path.join(self.root, data_file)
+        with open(path_to_data, 'rb') as f:
+            # read whole file in uint8 chunks
+            everything = np.fromfile(f, dtype=np.uint8)
+            images = np.reshape(everything, (-1, 3, 96, 96))
+            images = np.transpose(images, (0, 1, 3, 2))
+        return images
+        
 
     def __getitem__(self, idx):
-        img=Image.open(self.image_list[idx]).convert("RGB")
+        self.data = self.__loadfile(self.image_list[idx])
+        if self.labels is not None:
+            img, target = self.data[idx], int(self.labels[idx])
+        else:
+            img, target = self.data[idx], None
+        
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
+
         if self.transform is not None:
-                    img = self.transform(img)
-        image=[t.numpy() for t in img] 
-        image=np.array(image)
-        return image
+            img = self.transform(img)
+        return img
 
 class kangqiang_emb(Dataset):
     def __init__(self, root, split='train', show=False, transform1=None,transform2=None,embedding=None):
@@ -47,12 +68,12 @@ class kangqiang_emb(Dataset):
         
         with open(train_list_file,'r') as filehandle:
             for line in filehandle:
-                self.image_train_list.append(line)
+                self.image_train_list.append(line.splitlines())
             filehandle.close()
 
         with open(test_list_file,'r') as filehandle:
             for line in filehandle:
-                self.image_test_list.append(line)
+                self.image_test_list.append(line.splitlines())
             filehandle.close()
 
 
@@ -64,6 +85,7 @@ class kangqiang_emb(Dataset):
         # 返回数据集的数据数量
         return len(self.image_train_list),len(self.image_test_list)
 
+  
     def __loadfile(self, data_file):
         path_to_data = os.path.join(self.root, data_file)
         with open(path_to_data, 'rb') as f:
@@ -85,22 +107,25 @@ class kangqiang_emb(Dataset):
         # to return a PIL Image
         img = Image.fromarray(np.transpose(img, (1, 2, 0)))
         """
-        if self.labels is not None:
-            img, target = self.data[index], int(self.labels[index])
-        else:
-            img, target = self.data[index], None
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
-
+      
+        
         if self.split == 'train':
             self.data = self.__loadfile(self.image_train_list[idx])
         elif self.split == 'train+test':
             data_train = self.__loadfile(self.image_train_list[idx])
             data_test = self.__loadfile(self.image_test_list[idx])
             self.data = np.concatenate(data_train, data_test)
-       
+        
+        if self.labels is not None:
+            img, target = self.data[idx], int(self.labels[idx])
+        else:
+            img, target = self.data[idx], None
+        
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
+
+
         if self.embedding is not None:
             emb = self.embedding[idx]
         else:
@@ -121,22 +146,7 @@ class kangqiang_emb(Dataset):
         else:
             return img_trans1,img_trans2
 
-    def __loadfile(self, data_file, labels_file=None):
-        labels = None
-        if labels_file:
-            path_to_labels = os.path.join(
-                self.root, labels_file)
-            with open(path_to_labels, 'rb') as f:
-                labels = np.fromfile(f, dtype=np.uint8) - 1  # 0-based
-
-        path_to_data = os.path.join(self.root, data_file)
-        with open(path_to_data, 'rb') as f:
-            # read whole file in uint8 chunks
-            everything = np.fromfile(f, dtype=np.uint8)
-            images = np.reshape(everything, (-1, 3, 96, 96))
-            images = np.transpose(images, (0, 1, 3, 2))
-        return images, labels
-
+    
 if __name__ == '__main__':
     train_dataset = kangqiang_emb("/git/segment_images")
     num_samples=100
